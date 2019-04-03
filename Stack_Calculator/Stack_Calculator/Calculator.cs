313e3e4c66827c90d549eb7e3f0ffd3604f03bd9
KDM_korea <kdm_korea@naver.com>
2019-04-03 22:46:01 +0900

    [#1] Feat: Add postfix calculator code

---
+++ Stack_Calculator/Stack_Calculator/Calculator.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Stack_Calculator
+{
+    class Calculator
+    {
+        Convertingtype convert;
+
+        public double Postfix_Calculator(List<string> formula) {
+            double value = 0.0;
+            string op = string.Empty;
+            convert = new Convertingtype();
+            Stack<double> number = new Stack<double>();
+
+            for (int idx = 0; idx < formula.Count; idx++) {
+                if (convert.OperatorPriority(formula[idx].ToString()) == Sign.NotOperator) {
+                    number.Push(formula[idx].ToString().ParseToDouble());
+                }
+                else {
+                    op = formula[idx].ToString();
+                    value = ExecuteCalculator(number.Pop().ToString().ParseToDouble(), number.Pop().ToString().ParseToDouble(), op);
+                    number.Push(value);
+                }
+            }
+            return value;
+        }
+
+        private double ExecuteCalculator(double fstNum, double secNum, string oper) {
+            switch (oper) {
+                case "*":
+                    return fstNum * secNum;
+                case "/":
+                    return fstNum / secNum;
+                case "+":
+                    return fstNum + secNum;
+                case "-":
+                    return fstNum - secNum;
+                default:
+                    throw new Exception($"Unknoewn Operator '{oper}'");
+            }
+        }
+    }
+}
