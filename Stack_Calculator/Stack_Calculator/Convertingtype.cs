c356484ac65ada1eb6d21c80749141b9a017a9ae
KDM_korea <kdm_korea@naver.com>
2019-04-03 22:44:18 +0900

    [#1] Feat: Create project

---
+++ Stack_Calculator/Stack_Calculator/Convertingtype.cs
@@ -0,0 +1,87 @@
+﻿using System.Collections.Generic;
+using System.Text;
+
+namespace Stack_Calculator
+{
+    class Convertingtype
+    {
+        private Stack<string> operate = new Stack<string>();
+        private List<string> array = new List<string>();
+
+        private void PopOperator(StringBuilder currentData, int operKind, string oper) {
+            if (!currentData.ToString().Length.Equals(0)) {
+                array.Add(currentData.ToString());
+                currentData.Clear();
+            }
+            if (operKind == Sign.EndBracket) {
+                PopOperatorStack();
+            }
+            else {
+                PopOperatorStack(oper);
+            }
+            operate.Push(oper);
+        }
+
+        public List<string> ConvertPostfix(string formula) {
+            int operKind;
+            StringBuilder tmp = new StringBuilder();
+
+            for (int idx = 0; idx < formula.Length; idx++) {
+
+                operKind = OperatorPriority(formula[idx].ToString());
+                if (operKind == Sign.NotOperator) {
+                    tmp.Append(formula[idx]);
+                }
+                else {
+                    PopOperator(tmp, operKind, formula[idx].ToString());
+                }
+            }
+            array.Add(tmp.ToString());
+            array.Add(operate.Peek());
+            operate.Pop();
+
+            return array;
+        }
+
+        private void PopOperatorStack() {
+            for (int idx = 0; idx < operate.Count; idx++) {
+                if (OperatorPriority(operate.Peek().ToString()) != Sign.StartBracket) {
+                    array.Add(operate.Peek().ToString());
+                }
+                operate.Pop();
+            }
+        }
+
+        private void PopOperatorStack(string oper) {
+            for (int idx = 0; idx < operate.Count; idx++) {
+                if (OperatorPriority(operate.Peek().ToString()) <= OperatorPriority(oper) && OperatorPriority(operate.Peek().ToString()) > Sign.StartBracket) {
+                    array.Add(operate.Peek().ToString());
+                    operate.Pop();
+                }
+                else {
+                    operate.Pop();
+                    return;
+                }
+            }
+        }
+
+        public int OperatorPriority(string oper) {
+            switch (oper) {
+                case "*":
+                    return 3;
+                case "/":
+                    return 3;
+                case "+":
+                    return 2;
+                case "-":
+                    return 2;
+                case "(":
+                    return 1;
+                case ")":
+                    return 0;
+                default:
+                    return -1;
+            }
+        }
+    }
+}
---
+++ Stack_Calculator/Stack_Calculator/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Stack_Calculator
+{
+    class Program
+    {
+        static void Main(string[] args) {
+            Calculator execute = new Calculator();
+            Convertingtype convert = new Convertingtype();
+
+            string formula = "(32+2)*20/2";
+
+            double value = execute.Postfix_Calculator(convert.ConvertPostfix(formula));
+
+            Console.WriteLine(value);
+        }
+    }
+}
