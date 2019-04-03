762648d2618bb94e3aca5a3cf8ffbefebffe9aa0
KDM_korea <kdm_korea@naver.com>
2019-04-03 22:45:25 +0900

    [#1] Feat: Operator Sign add

---
+++ Stack_Calculator/Stack_Calculator/Sign.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Stack_Calculator
+{
+    public static class Sign
+    {
+        public static readonly int NotOperator = -1;
+        public static readonly int StartBracket = 1;
+        public static readonly int EndBracket = 0;
+    }
+}
