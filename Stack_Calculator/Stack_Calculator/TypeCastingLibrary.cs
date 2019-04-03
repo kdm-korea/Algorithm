4bdf83eb61133d51da35a62885a6cbb471a273b2
KDM_korea <kdm_korea@naver.com>
2019-04-03 22:44:58 +0900

    [#1] Feat: Converting type cast

---
+++ Stack_Calculator/Stack_Calculator/TypeCastingLibrary.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Stack_Calculator
+{
+    public static class TypeCastingLibrary
+    {
+        public static double ParseToDouble(this string s) {
+            double.TryParse(s, out double value);
+            return value;
+        }
+    }
+}
