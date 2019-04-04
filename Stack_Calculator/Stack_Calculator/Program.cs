48bc1b7601f5e99e3d915506106d0fd3c2e5f8c2
kdm-korea <kdm_korea@naver.com>
2019-04-04 16:48:14 +0900

    [#2] Refector: retrenchment code line / Print formula

--- Stack_Calculator/Stack_Calculator/Program.cs
+++ Stack_Calculator/Stack_Calculator/Program.cs
@@ -4,15 +4,30 @@ namespace Stack_Calculator
 {
     class Program
     {
+        static Convertingtype convert;
+
         static void Main(string[] args) {
             Calculator execute = new Calculator();
-            Convertingtype convert = new Convertingtype();
+            convert = new Convertingtype();
 
             string formula = "(32+2)*(20/2)+3*4+2";
 
-            double value = execute.Postfix_Calculator(convert.ConvertPostfix(formula));
+            Console.WriteLine($"Infix_Formula : {formula}");
 
-            Console.WriteLine(value);
+            Prt_PostfixFormula(formula);
+
+            Console.WriteLine($"Value : {execute.Postfix_Calculator(convert.ConvertPostfix(formula))}");
+        }
+
+        private static void Prt_PostfixFormula(string formula) {
+            convert = new Convertingtype();
+            var tmp = convert.ConvertPostfix(formula);
+
+            Console.Write("Postfix_Formula : ");
+            foreach (var item in tmp) {
+                Console.Write($"{item} ");
+            }
+            Console.WriteLine();
         }
     }
 }
