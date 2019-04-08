dde96134841fdfce42356df5183e8c7e8323f291
KDM_korea <kdm_korea@naver.com>
2019-04-08 22:10:12 +0900

    [#4] Feat: Circle Queue need method make

---
+++ CircleQueue/CircleQueue/ICircleQueue.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CircleQueue
+{
+    interface ICircleQueue<T>
+    {
+        void Enqueue(T item);
+        void Dequeue();
+        void Clear();
+        int Count();
+        T Peek();
+        int Rear();
+        int Front();
+    }
+}
