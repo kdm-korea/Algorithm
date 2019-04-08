d862e6a37553f33296f962b1ecd5c8d6da75f386
KDM_korea <kdm_korea@naver.com>
2019-04-08 22:12:13 +0900

    Feat: Create CircleQueue Object

---
+++ CircleQueue/CircleQueue/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace CircleQueue
+{
+    class Program
+    {
+        static void Main(string[] args) {
+            CircleQueue<string> circleQueue = new CircleQueue<string>(3);
+        }
+
+       
+    }
+}
