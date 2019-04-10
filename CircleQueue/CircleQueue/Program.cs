86ed2a1afd4e47a447bbb942731011a55644a018
KDM_korea <kdm_korea@naver.com>
2019-04-10 19:15:54 +0900

    [#6] Feat: Debug code
    원형 큐 실행 확인 코드

--- CircleQueue/CircleQueue/Program.cs
+++ CircleQueue/CircleQueue/Program.cs
@@ -5,10 +5,44 @@ namespace CircleQueue
 {
     class Program
     {
+        public static CircleQueue<string> circleQueue;
+
         static void Main(string[] args) {
-            CircleQueue<string> circleQueue = new CircleQueue<string>(3);
+            circleQueue = new CircleQueue<string>(4);
+
+            circleQueue.Enqueue("김동민1");
+            circleQueue.Enqueue("김동민2");
+            circleQueue.Enqueue("김동민3");
+            circleQueue.Enqueue("김동민4");
+            PrtQueue("첫번째 출력", circleQueue);
+
+            circleQueue.Dequeue();
+            PrtQueue("두번째 출력", circleQueue);
+
+            circleQueue.Enqueue("김동민5");
+            PrtQueue("세번째 출력", circleQueue);
+
+            circleQueue.Dequeue();
+            PrtQueue("네번째 출력", circleQueue);
+
+            circleQueue.Enqueue("김동민6");
+            PrtQueue("다섯번째 출력", circleQueue);
+
+            circleQueue.Dequeue();
+            PrtQueue("여섯번째 출력", circleQueue);
+
+            circleQueue.Enqueue("김동민7");
+            PrtQueue("일곱번째 출력", circleQueue);
         }
 
-       
+        public static void PrtQueue(string count, CircleQueue<string> circleQueue) {
+            Console.WriteLine(count);
+            Console.WriteLine("==============================");
+            for (int idx = 0; idx < circleQueue.Length; idx++) {
+                Console.WriteLine(circleQueue[idx]);
+            }
+            Console.WriteLine("==============================");
+            Console.WriteLine();
+        }
     }
 }
