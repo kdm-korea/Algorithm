466c7ac0e708a508fa208071b91a042b9ade7abe
KDM_korea <kdm_korea@naver.com>
2019-04-10 19:14:38 +0900

    [#7] Feat: Exception matter untangle
    OutofRange Error과 Nullpointer Error를 처리하기 위한 사용자 exception 클래스 생성

---
+++ CircleQueue/CircleQueue/CustomException.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CircleQueue
+{
+    class CustomException : Exception
+    {
+        public CustomException() : base() {
+        }
+
+        public CustomException(string message) : base(message) {
+            Console.WriteLine(message);
+        }
+
+        public CustomException(string message, Exception innerException) : base(message, innerException) {
+            Console.WriteLine(message);
+        }
+    }
+}
