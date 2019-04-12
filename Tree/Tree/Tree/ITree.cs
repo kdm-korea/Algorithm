febf3049dd9fd95103f78fc102e96f481fe5edd8
KDM_korea <kdm_korea@naver.com>
2019-04-12 22:11:49 +0900

    [#8] Create: BinaryTree Architecture

---
+++ Tree/Tree/Tree/ITree.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tree
+{
+    interface ITree<T>
+    {
+        bool Create(T root);
+        bool Insert(T parent, T child);
+        bool Find(T value);
+        bool Delete(T value);
+    }
+}
---
+++ Tree/Tree/Tree/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tree
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            TreeNode<string> treeNode = new TreeNode<string>();
+
+            
+
+        }
+    }
+}
