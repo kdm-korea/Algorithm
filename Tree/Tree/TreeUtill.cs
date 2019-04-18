10fc28c704f70fabda2eb2522652984bd82380d0
kdm-korea <kdm_korea@naver.com>
2019-04-18 14:36:10 +0900

    [#9] Feat: nullreference check..

---
+++ Tree/Tree/TreeUtill.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace Tree
+{
+    class TreeUtill<T>
+    {
+        public void PrtNullRender(Node<T> node) {
+            Console.WriteLine();
+            Console.WriteLine("==============================");
+            try {
+                Console.WriteLine($"node.Data :: {node.Data}");
+            }
+            catch (NullReferenceException) {
+                Console.WriteLine($"node.Data :: null");
+            }
+
+            try {
+                Console.WriteLine($"node.Right.Data :: {node.Right.Data}");
+            }
+            catch (NullReferenceException) {
+                Console.WriteLine($"node.Right.Data :: null");
+            }
+
+            try {
+                Console.WriteLine($"node.Left.Data :: {node.Left.Data}");
+            }
+            catch (NullReferenceException) {
+                Console.WriteLine($"node.Left.Data :: null");
+            }
+            Console.WriteLine("==============================");
+        }
+    }
+}
