9ddd28dc8ad76a9efc048000d7a412a4fcc74f1c
kdm-korea <kdm_korea@naver.com>
2019-04-18 14:35:37 +0900

    [#9] Feat: BinaryTree implement

---
+++ Tree/Tree/BinaryTree.cs
@@ -0,0 +1,75 @@
+﻿using System;
+
+namespace Tree
+{
+    class BinaryTree<T> : ITree<T, Node<T>>
+    {
+        private Node<T> root;
+        private Node<T> resultNode = default(Node<T>);
+
+        public BinaryTree(T root) {
+            this.root = new Node<T> { Data = root };
+        }
+
+        public bool Delete(T data) {
+            try {
+                DeleteRecurrent(Retreive(data));
+            }
+            catch (Exception) {
+                return false;
+            }
+            return true;
+        }
+
+        private void DeleteRecurrent(Node<T> node) {
+            if (node != default(Node<T>)) {
+                Initialization(node);
+                DeleteRecurrent(node.Left);
+                DeleteRecurrent(node.Right);
+
+                if (node.Left != default(Node<T>) && node.Right != default(Node<T>)) {
+                    return;
+                }
+            }
+        }
+
+        public Node<T> Retreive(T data) {
+            PreOrderRecurrent(root, data);
+            if (data.Equals(resultNode.Data)) {
+                return resultNode;
+            }
+            return default(Node<T>);
+        }
+
+        public bool Insert(Node<T> node, T data, Direct direct) {
+            if (direct.Equals(Direct.Left)) {
+                node.Left = new Node<T> { Data = data, };
+                return true;
+            }
+            else if (direct.Equals(Direct.Right)) {
+                node.Right = new Node<T> { Data = data };
+                return true;
+            }
+            return false;
+        }
+
+        public Node<T> Root() => this.root;
+
+        private void Initialization(Node<T> node) {
+            node.Data = default(T);
+            node.Left = default(Node<T>);
+            node.Right = default(Node<T>);
+        }
+
+        private void PreOrderRecurrent(Node<T> node, T data) {
+            if (node != default(Node<T>)) {
+                PreOrderRecurrent(node.Left, data);
+                PreOrderRecurrent(node.Right, data);
+
+                if (node.Data.Equals(data)) {
+                    resultNode = node;
+                }
+            }
+        }
+    }
+}
