fed15ed2b99df03dcb45c14e1c557b422ded836a
kdm-korea <kdm_korea@naver.com>
2019-04-18 14:35:01 +0900

    [#8] Post: interface architect

---
+++ Tree/Tree/Node.cs
@@ -0,0 +1,10 @@
+﻿
+namespace Tree
+{
+    public class Node<T>
+    {
+        public T Data { get; set; }
+        public Node<T> Left { get; set; }
+        public Node<T> Right { get; set; }
+    }
+}
