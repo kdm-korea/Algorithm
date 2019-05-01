63f98ce4d6a456b662caef3c7cecbd0e74e697a1
kdm-korea <kdm_korea@naver.com>
2019-05-01 17:12:20 +0900

    [#12] Feat: model implement

--- Heap_Sort/Heap_Sort/MaxinumHeap.cs
+++ Heap_Sort/Heap_Sort/MaxinumHeap.cs
@@ -4,18 +4,57 @@ namespace Heap_Sort
 {
     class MaxinumHeap : IHeap
     {
-        private int[] heap = new int[] { };
+        public int[] heap = new int[] { };
+        private int rData = default, lData = default;
+        private int idx = 1;
 
-        public bool Delete(int data) {
+        public MaxinumHeap(int heap) {
+            this.heap[idx] = heap;
+            idx++;
+        }
+
+        public void Delete(int data) {
             throw new NotImplementedException();
         }
 
-        public bool Insert(int data) {
-            throw new NotImplementedException();
+        public void Insert(int data) {
+            int tmp = idx;
+            heap[idx] = data;
+            while (tmp > 0) {
+                if(Compare(FindParent(tmp), heap[idx])) {
+                    Swap(FindParent(tmp), heap[idx]);
+                }
+            }
+            idx++;
         }
 
-        public bool Update(int delData, int addData) {
-            throw new NotImplementedException();
+        private int FindParent(int index) {
+            return (heap[index] % 2 == 1) ? heap[(index - 1) / 2] : heap[index / 2];
+        }
+
+        private bool Compare(int fst, int sec) {
+            return (fst > sec) ? true : false;
+        }
+
+        private void Swap(int fst, int sec) {
+            int tmp = fst;
+            fst = sec;
+            sec = tmp;
+        }
+
+        //private int FindChild(int data) {
+        //    try {
+
+        //    }
+        //    catch (IndexOutOfRangeException) {
+
+        //        throw;
+        //    }
+        //}
+
+        private void InintChild() {
+            rData = default;
+            lData = default;
         }
     }
 }
