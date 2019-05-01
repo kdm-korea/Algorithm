bdd767c07a0485c3428cc1ab1e78b427c29cbb8d
kdm-korea <kdm_korea@naver.com>
2019-05-01 17:12:01 +0900

    [#12] Feat: make view

--- Heap_Sort/Heap_Sort/Program.cs
+++ Heap_Sort/Heap_Sort/Program.cs
@@ -8,7 +8,23 @@ namespace Heap_Sort
 {
     class Program
     {
+        static MaxinumHeap maxHeap;
+
         static void Main(string[] args) {
+            maxHeap = new MaxinumHeap(1);
+
+            maxHeap.Insert(10);
+            maxHeap.Insert(13);
+            maxHeap.Insert(9);
+            maxHeap.Insert(6);
+            maxHeap.Insert(1);
+            maxHeap.Insert(15);
+
+            maxHeap.Delete(10);
+
+            foreach (var i in maxHeap.heap) {
+                Console.WriteLine(i);
+            }
         }
     }
 }
