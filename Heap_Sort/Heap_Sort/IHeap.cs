cb29d80dbcbdc11834306e81bb0dfd9cb51f9830
kdm-korea <kdm_korea@naver.com>
2019-05-01 16:33:12 +0900

    [#11] Feat: Heap Architecture

---
+++ Heap_Sort/Heap_Sort/IHeap.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Heap_Sort
+{
+    interface IHeap
+    {
+        bool Insert(int data);
+        bool Update(int delData, int addData);
+        bool Delete(int data);
+    }
+}
---
+++ Heap_Sort/Heap_Sort/MaxinumHeap.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Heap_Sort
+{
+    class MaxinumHeap : IHeap
+    {
+        private int[] heap = new int[] { };
+
+        public bool Delete(int data) {
+            throw new NotImplementedException();
+        }
+
+        public bool Insert(int data) {
+            throw new NotImplementedException();
+        }
+
+        public bool Update(int delData, int addData) {
+            throw new NotImplementedException();
+        }
+    }
+}
