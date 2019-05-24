72bca838b7d3faf4f3b15dacce165921fbd848ec
KDM_korea <kdm_korea@naver.com>
2019-05-24 11:45:15 +0900

    Feat: Topological Sorting algorithm

---
+++ Topological_Sort/Topological_Sort/Paragraph.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections.Generic;
+
+namespace Topological_Sort
+{
+    class Paragraph
+    {
+        public List<string> list = new List<string>();
+    }
+}
---
+++ Topological_Sort/Topological_Sort/TopoloicalSort.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Topological_Sort
+{
+    class TopoloicalSort
+    {
+        Paragraph paragraph;
+        private List<Paragraph> result = new List<Paragraph>();
+
+        public void Execute_TopoloicalSort(Dictionary<string, int> DAG_InDegree) {
+            while (DAG_InDegree.Count != 0) {
+                BFS_Polling(DAG_InDegree);
+            }
+        }
+
+        public void Prt_TopologicalSort() {
+            foreach (var collect in result) {
+                Console.WriteLine("================================================");
+                foreach (var val in collect.list) {
+                    Console.Write($"{val}   ");
+                }
+                Console.WriteLine();
+            }
+        }
+
+        private void BFS_Polling(Dictionary<string, int> DAG_InDegree) {
+            paragraph = new Paragraph();
+            foreach (string key in DAG_InDegree.Keys.ToList()) {
+                if (ChkInDegreeZero(DAG_InDegree, key)) {
+                    Insert_BFS_Queue(DAG_InDegree, key);
+                }
+                else {
+                    Polling(DAG_InDegree, key);
+                }
+            }
+            result.Add(paragraph);
+        }
+
+        private bool ChkInDegreeZero(Dictionary<string, int> DAG_InDegree, string key) => DAG_InDegree[key] == 0 ? true : false;
+
+        void Insert_BFS_Queue(Dictionary<string, int> DAG_InDegree, string value) {
+            paragraph.list.Add(value);
+            DAG_InDegree.Remove(value);
+        }
+
+        private void Polling(Dictionary<string, int> DAG_InDegree, string key) => DAG_InDegree[key]--;
+    }
+}
