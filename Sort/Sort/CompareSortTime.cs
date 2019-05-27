5b459a0feeb02ff428202f7caf19da28e84e0f8b
kdm-korea <kdm_korea@naver.com>
2019-05-27 17:14:52 +0900

    Feat: Compare to quicksort and c_shap library sort way timestamp

---
+++ Sort/Sort/CompareSortTime.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace Sort
+{
+    public class CompareSortTime
+    {
+        public List<int> quick_SortNum = new List<int>();
+        public List<int> cShap_SortNum = new List<int>();
+
+        public bool CollectRandomNumber() {
+            RandomNumber randomNumber = new RandomNumber();
+
+            quick_SortNum = randomNumber.Create_RandomNumber(); // Create Random Integer Number 1 ~ 100,000
+            quick_SortNum = cShap_SortNum.ToList();             // Deep copy
+            return true;
+        }
+
+        public bool C_ShapLibrarySort(List<int> num) {
+            try {
+                num.Sort();
+                return true;
+            }
+            catch (System.Exception) {
+                return false;
+            }
+        }
+
+        public bool Custom_QuickSort(List<int> num) {
+            return true;
+        }
+
+    }
+}
---
+++ Sort/Sort/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sort
+{
+    class Program
+    {
+        static CompareSortTime compareSortTime = new CompareSortTime();
+
+        static void Main(string[] args) {
+            compareSortTime.CollectRandomNumber();
+            
+
+        }
+
+        
+
+       
+    }
+}
